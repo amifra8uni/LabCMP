bf8c688d75b0971e5ca5d82905a6ba42d2e2e158
mattderri <92681931+mattderri@users.noreply.github.com>
2021-10-18 10:16:27 +0200

    Add files via upload

---
+++ Complex.h
@@ -0,0 +1,93 @@
+# include <iostream>
+using namespace std;
+
+class Complex{
+  
+private:
+  double real,imag;
+
+public:
+  Complex(){
+    real=0;
+    imag=0;
+  }
+
+  Complex(double r, double i){
+    real=r;
+    imag=i;
+  }
+  
+  Complex(Complex &obj){
+    real=obj.real;
+    imag=obj.imag;
+  }
+
+  Complex add(Complex c){
+    Complex Add;
+    Add.real=real+c.real;
+    Add.imag=imag+c.imag;
+    return Add;
+  }
+
+  Complex sub(Complex c){
+    Complex Sub;
+    Sub.real=real-c.real;
+    Sub.imag=imag-c.imag;
+    return Sub;
+  }
+
+  Complex mult(Complex c){
+    Complex Mult;
+    Mult.real=real*c.real-imag*c.imag;
+    Mult.imag=real*c.imag-imag*c.real;
+    return Mult;
+  }
+
+  Complex div(Complex c){
+    Complex Div;
+    Div.real=(real*c.real+imag*c.imag)/(c.real*c.real+c.imag*c.imag);
+    return Div;
+  }
+
+  void print(){
+    cout<<real<<"+"<imag<<"i"<<endl<<endl;
+  }
+
+  double re() const{
+    return real;
+  }
+
+  double im() const{
+    return imag;
+  }
+
+  doulbe r() const{
+    return sqrt(real*real+imag*imag);
+  }
+
+  double phi() const{
+    return imag/real;
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+  
