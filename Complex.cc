1afbed10b8849359bb90146200afab2f50956bec
Student of Computing Methods for Physics <student@localhost.localdomain>
2021-10-19 21:28:46 +0200

    Prova da casa, non completo

---
+++ Complex.cc
@@ -0,0 +1,13 @@
+/* Complex.cc
+Implementazione della classe Complex
+
+Includo l'archivio header della classe*/
+#Include "Complex.h"
+
+//Includo tutti gli archivi aggiuntivi necessari
+#Include <iostream>
+using std::cout;
+using std::endl;
+
+// constructor
+Complex::Complex
