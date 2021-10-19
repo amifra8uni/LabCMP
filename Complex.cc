a0129d921a568864a04b2aa9c9464a2bff641f74
Francesco Amicucci <amicucci.1795152@studenti.uniroma1.it>
2021-10-19 23:20:33 +0200

    Aggiunti i tre tipi di costruttori della lezione 3

--- Complex.cc
+++ Complex.cc
@@ -2,6 +2,40 @@
 Implementazione della classe Complex
 
 Includo l'archivio header della classe*/
+#include "Complex.h"
+
+//Includo tutti gli archivi aggiuntivi necessari
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// constructor o costruttori
+Complex::Complex() { }
+Complex::Complex(double real, double imag) {
+  real_=real;
+  imag_=imag;
+}
+Complex::Complex(const Complex& complex) {
+  real_=complex.real_;
+  imag_=complex.imag;
+}
+
+//getters o funzioni d'accesso
+double Complex::real() { return real_; }
+double Complex::imag() { return imag_; }
+
+//setters o funzioni d'attualizzamento
+void Complex::setReal(double real) { real_ = real; }
+void Complex::setImag(double imag) { imag_ = imag; }
+
+void Complex::print() {
+  cout << "Numero complesso: " << real_ << "+ " << imag_ << "i";
+}
+
+/* Complex.cc
+Implementazione della classe Complex
+
+Includo l'archivio header della classe*/
 #Include "Complex.h"
 
 //Includo tutti gli archivi aggiuntivi necessari
