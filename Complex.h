f3c43e181b01941ceaa4391946b354146e9e4df7
Francesco Amicucci <amicucci.1795152@studenti.uniroma1.it>
2021-10-23 09:49:27 +0200

    Aggiornto

--- Complex.h
+++ Complex.h
@@ -1,95 +1,24 @@
-//AO DEVI DA FUNZIONARE
+/*Complex.h
+  Archivio di intestazione (o header file) della classe Complex*/
 
-# include <iostream>
-using namespace std;
+class Complex {
+ public:
+  // constructors o costruttori
+  Complex ();
+  Complex(double real, double imag);
+  Complex(const Complex& complex);
 
-class Complex{
-  
-private:
-  double real,imag;
+  //getters o funzioni d'accesso
+  double real();
+  double imag();
 
-public:
-  Complex(){
-    real=0;
-    imag=0;
-  }
+  //setters o funzioni d'attualizzamento
+  void setReal(double real);
+  void setImag(double imag);
 
-  Complex(double r, double i){
-    real=r;
-    imag=i;
-  }
-  
-  Complex(Complex &obj){
-    real=obj.real;
-    imag=obj.imag;
-  }
+  void print();
 
-  Complex add(Complex c){
-    Complex Add;
-    Add.real=real+c.real;
-    Add.imag=imag+c.imag;
-    return Add;
-  }
-
-  Complex sub(Complex c){
-    Complex Sub;
-    Sub.real=real-c.real;
-    Sub.imag=imag-c.imag;
-    return Sub;
-  }
-
-  Complex mult(Complex c){
-    Complex Mult;
-    Mult.real=real*c.real-imag*c.imag;
-    Mult.imag=real*c.imag-imag*c.real;
-    return Mult;
-  }
-
-  Complex div(Complex c){
-    Complex Div;
-    Div.real=(real*c.real+imag*c.imag)/(c.real*c.real+c.imag*c.imag);
-    return Div;
-  }
-
-  void print(){
-    cout<<real<<"+"<imag<<"i"<<endl<<endl;
-  }
-
-  double re() const{
-    return real;
-  }
-
-  double im() const{
-    return imag;
-  }
-
-  doulbe r() const{
-    return sqrt(real*real+imag*imag);
-  }
-
-  double phi() const{
-    return imag/real;
-  }
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-
-  
+ private:
+  double real_; 
+  double imag_;
+};
