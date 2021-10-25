9b04aca8e426fb078cbb34612103d9beed4d0369
Francesco Amicucci <amicucci.1795152@studenti.uniroma1.it>
2021-10-25 09:53:07 +0200

    Funzioni per vedere se i setters e le member functions funzionano

--- Vector3D.cpp
+++ Vector3D.cpp
@@ -39,14 +39,27 @@ int main() {
   for(Vector3D vettore_itr : vettore) {
     i++;
     cout << "vettore numero " << i << ": " << "\t vettore: { "
-	 << vettore_itr.x() << ", "
-	 << vettore_itr.y() << ", "
-	 << vettore_itr.z() << " }" << endl;
+	 << vettore_itr.getX() << ", "
+	 << vettore_itr.getY() << ", "
+	 << vettore_itr.getZ() << " }" << endl;
   }
 
   // Verifica funzionamento setters
   vettore.push_back( vettore(0.,0.,0.) );
   cout << "provo a inserire un vettore 1,1,1" << endl;
-  
+  cout << "vettore numero 3" << "\t vettore: { "
+       << vettore[2].getX() << ", "
+       << vettore[2].getY() << ", "
+       << vettore[2].getZ() << " }" << endl;
+
+  // Verifica funzionamento member functions
+  for(Vector3D vettore_itr : vettore) {
+    i++;
+    cout << "vettore numero " << i << "\n"
+	 << "Magnitudine: " << vettore_itr.magnitude()
+	 << "Teta: "        << vettore_itr.theta()
+	 << "Phi: "          << vettore_itr.phi()
+      
+  }
   return 0;
 }
