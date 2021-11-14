0ba5ee85da1144e8484a7988b1bb7cf3cc674b0a
Francesco Amicucci <amifra8@pc-fisso-ubuntu.fritz.box>
2021-11-14 20:17:37 +0100

    Aggiunta grafici per diverse risoluzioni

--- decadimento_Fra.cpp
+++ decadimento_Fra.cpp
@@ -11,11 +11,25 @@
 #include "TFile.h"
 #include "TLorentzVector.h"
 #include "TMath.h"
+#include "THStack.h"
+#include "Vector3D.h"
 
 using namespace std;
 
 // For the time being we will not use command line variables
 int main() {
+
+    // Open TFile for output
+  TString rootfname("./output.root");
+  // Overwite output file if it already exists
+  TFile rfile(rootfname, "RECREATE");
+  // Open the ROOT file and make sure the file was opened successfully.
+  // Typical cause for failure are: 1) wrong path, 2) no write privilege
+  if( !rfile.IsOpen() ) {
+    std::cout << "problems creating root file. existing... " << std::endl;
+    exit(-1);
+  }
+  std::cout << "storing output in root file " << rootfname << std::endl;
 
   /*Con un generatore di numeri casuali, vogliamo
     1) Generale il VALORE VERO dell'osservabile in un determinato intervallo e poi
@@ -43,15 +57,26 @@ int main() {
   double p = sqrt( M_B*M_B*M_B*M_B + m_pi*m_pi*m_pi*m_pi + m_K*m_K*m_K*m_K - 2*M_B*M_B*m_pi*m_pi - 2*M_B*M_B*m_K*m_K ) / (2*M_B); // GeV
 
   // Istogramma della massa invariante (name, title, number of bins, range)
-  int    nbins = 1000;
-  double resol = 0.03;
-  double xlo   = M_B*(1-5*resol);
-  double xhi   = M_B*(1+5*resol);
+  int    nbins = 200;
+  double resol = 0.1;
+  double xlo   = M_B*(1-3*resol);
+  double xhi   = M_B*(1+3*resol);
   double binwidth = (xhi-xlo) / nbins;
-  TH1F hs("hs", "Distribuzione della massa invariante", nbins, xlo, xhi);
+  vector<TH1F> histo;
+  vector<short int> colore = {kBlue, kRed, kGreen, kYellow};
+  THStack hs("hs", "Sovrimpressione s teorico e misurato");
+  THStack hsris("hsris", "Sovrimpressione s misurato per diverse risoluzioni");
+  TH1F hs0("hs", "Distribuzione della massa invariante", nbins, xlo, xhi);
   TH1F hangle("hangle", "Distribuzione dell'angolo di apertura tra K e pi", nbins, 0, 180);
   // The 'd' is for detector
-  TH1F hds("hds", "Distribuzione della massa invariante misurata", nbins, xlo , xhi);
+  /*TH1F hds1( "hds1"  , "Distribuzione della massa invariante misurata", nbins, xlo , xhi);
+  TH1F hds3( "hds3"  , "Distribuzione della massa invariante misurata", nbins, xlo , xhi);
+  TH1F hds5( "hds5"  , "Distribuzione della massa invariante misurata", nbins, xlo , xhi);
+  TH1F hds10("hds10" , "Distribuzione della massa invariante misurata", nbins, xlo , xhi);*/
+  histo.push_back(TH1F( "hds1" , "Distribuzione della massa invariante misurata", nbins, xlo , xhi) );
+  histo.push_back(TH1F( "hds3" , "Distribuzione della massa invariante misurata", nbins, xlo , xhi) );
+  histo.push_back(TH1F( "hds5" , "Distribuzione della massa invariante misurata", nbins, xlo , xhi) );
+  histo.push_back(TH1F("hds10" , "Distribuzione della massa invariante misurata", nbins, xlo , xhi) );
   /* Genero 10^4 direzioni casuali
      As a reminder:
      * Hadron colliders measure physical momenta in terms of momentum transverse
@@ -63,9 +88,10 @@ int main() {
        where m_T = sqrt(p_T^2 + m^2) is the TRANSVERSE MASS
      * eta is the PSEUDORAPIDITY: eta = -ln[tan(theta/2)]; differences in eta
        are Lorentz invariants under boosts along the longitudinal axis */ 
-  for(int i = 0; i < 10; i++) {
+  for(int i = 0; i < 10000; i++) {
     // Genero un vettore uniformemente distribuito sulla superficie di una sfera unitaria
-    double x, y, z, s_0, s_mis, p_K_0, p_pi_0, p_K_meas, p_pi_meas;
+    double x, y, z, s_0, p_K_0, p_pi_0, p_K_meas, p_pi_meas;
+    double ris[4] = {0.01, 0.03, 0.05, 0.1};
     gen->Sphere(x, y, z, 1);
     // Flat metric, (- - - +) signature: m^2 = E^2 - p^2
     // CdM
@@ -79,48 +105,66 @@ int main() {
     //Calcolo massa invariante
     TLorentzVector p4_somma_0 = p4_K + p4_pi;
     s_0 = sqrt( ( p4_K.E() + p4_pi.E() )*( p4_K.E() + p4_pi.E() ) - p4_somma_0.P()*p4_somma_0.P() );
-    hs.Fill(s_0);
+    hs0.Fill(s_0);
     hangle.Fill(p4_K.Angle(p4_pi.Vect())*180/M_PI);
 
     p_K_0  = p4_K.P();
     p_pi_0 = p4_pi.P();
     // Generate MEASURED value based on the true value and resolution
-    p_K_meas = gen->Gaus(p_K_0, p_K_0*resol);
-    p_K_meas = gen->Gaus(p_K_0, p_K_0*resol);
-    /*
-    double phi = p4_pi.Phi(), eta = p4_pi.Eta();
-    p4_pi.SetPxPyPzE( p_pi_meas*cos(phi),  p_pi_meas*sin(phi),  p_pi_meas*cosh(eta), sqrt(m_pi*m_pi + p_pi_meas*p_pi_meas));
-    phi = p4_K.Phi();
-    eta = p4_K.Eta();
-    p4_K.SetPxPyPzE ( p_K_meas*cos( phi) ,  p_K_meas*sin( phi) ,  p_K_meas*cosh( eta) , sqrt(m_K*m_K   + p_K_meas*p_K_meas  ));*/
-
-    p4_pi.SetPxPyPzE( p_pi_meas*sin(p4_pi.Theta())*cos(p4_pi.Phi()),  p_pi_meas*sin(p4_pi.Theta())*sin(p4_pi.Phi()),  p_pi_meas*cos(p4_pi.Theta()), sqrt(m_pi*m_pi + p_pi_meas*p_pi_meas));
-    p4_K.SetPxPyPzE ( p_K_meas*sin(p4_K.Theta()  )*cos( p4_K.Phi()) , p_K_meas*sin( p4_K.Theta() )*sin( p4_K.Phi()) , p_K_meas*cos( p4_K.Theta()) , sqrt(m_K*m_K   + p_K_meas*p_K_meas  ));
-    //Calcolo massa invariante misurata
-    TLorentzVector p4_somma_mis = p4_K + p4_pi;
-    s_mis = sqrt( ( p4_K.E() + p4_pi.E() )*( p4_K.E() + p4_pi.E() ) - p4_somma_mis.P()*p4_somma_mis.P() );
-    cout << s_mis << endl;
-    hds.Fill(s_mis);
+    for(int j = 0; j < histo.size(); j++) {
+      p_pi_meas = gen->Gaus(p_pi_0, p_pi_0*ris[j]);
+      p_K_meas  = gen->Gaus(p_K_0,  p_K_0*ris[j] );
+      p4_pi.SetPxPyPzE( p_pi_meas*sin(p4_pi.Theta())*cos(p4_pi.Phi()),  p_pi_meas*sin(p4_pi.Theta())*sin(p4_pi.Phi()),  p_pi_meas*cos(p4_pi.Theta()), sqrt(m_pi*m_pi + p_pi_meas*p_pi_meas));
+      p4_K.SetPxPyPzE ( p_K_meas*sin(p4_K.Theta()  )*cos( p4_K.Phi()) , p_K_meas*sin( p4_K.Theta() )*sin( p4_K.Phi()) , p_K_meas*cos( p4_K.Theta()) , sqrt(m_K*m_K   + p_K_meas*p_K_meas  ));
+      //Calcolo massa invariante misurata
+      TLorentzVector p4_somma_mis = p4_K + p4_pi;
+      double s_mis = sqrt( ( p4_K.E() + p4_pi.E() )*( p4_K.E() + p4_pi.E() ) - p4_somma_mis.P()*p4_somma_mis.P() );
+      histo[j].Fill(s_mis);
+    }
   }
 
   // After generating the data, we take care of plotting the results:
   // * create canvas
   TCanvas canv("canv", "canvas for plotting", 1280, 1024);
   // * start from TRUE values and customize some histogram properties
-  hs.GetXaxis()->SetTitle( "Distribuzione di s [GeV]");
-  hds.GetXaxis()->SetTitle("Distribuzione di s misurata [GeV]");
+  hs0.GetXaxis()->SetTitle( "Distribuzione di s [GeV]");
+  histo[1].GetXaxis()->SetTitle("Distribuzione di s misurata [GeV]");
   hangle.GetXaxis()->SetTitle("Distribuzione angolare [°]");
   // * plot e store to file in 1 formats
-  hs.Draw();
+  hs0.Draw();
   canv.SaveAs("./true-mass.pdf");
-  hds.Draw();
+  histo[0].Draw();
+  canv.SaveAs("./measured-mass-1per.pdf");
+  histo[1].Draw();
   canv.SaveAs("./measured-mass.pdf");
   hangle.Draw();
   canv.SaveAs("./opening-angle.pdf");
+
+  hs0.SetFillColor(kRed);
+  hs.Add(&hs0);
+  histo[1].SetFillColor(kBlue);
+  hs.Add(&histo[1]);
+  hs.Draw();
+  canv.SaveAs("./invariant-mass.pdf");
+
+  for(int k = 0; k < colore.size(); k++) {
+    histo[k].SetFillColor(colore[k]);
+    hsris.Add(&histo[k]);
+  }
+  hsris.Draw();
+  canv.SaveAs("./invariant-masses.pdf");
   // Delete the random generator now we are done with it
   // [We had new, here is delete!]
   delete gen;
 
+  // Salve entrambi gli istrogrammi in un archivio
+  hs0.Write();
+  histo[1].Write();
+  hangle.Write();
+  
+  // Critical to close the file!
+  rfile.Close();
+  
   // Exit
   return 0;
 }
